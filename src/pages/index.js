ba6b3f311e751064f2df1e4ce93d749ae4ad9f72
Nathan Patterson <nathan.e.patterson@gmail.com>
2021-11-10 16:54:06 +0900

    update footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -55,14 +55,11 @@ export default function Home() {
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://www.n8patterson.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+          @ 2021 Nathan Patterson
         </a>
       </footer>
     </div>
